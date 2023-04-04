6a0a5728200dc509cc0cc5d9d1439256634c22f8
Code Sandbox <hello@codesandbox.io>
2023-04-04 17:50:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6538b4a6078a9cd0f6aa1563a07c7c24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb64a46d37a651fcc34c60b27f875f7",
+    "url": "https://.github.io/csb-jrxwhl/index.html"
+  },
+  {
+    "revision": "26fbe00796a828592c2b",
+    "url": "https://.github.io/csb-jrxwhl/static/js/2.3c57d551.chunk.js"
+  },
+  {
+    "revision": "db6e7484050de87780d9",
+    "url": "https://.github.io/csb-jrxwhl/static/js/main.6530c100.chunk.js"
+  },
+  {
+    "revision": "f4fafdcbdde524ab0c31",
+    "url": "https://.github.io/csb-jrxwhl/static/js/runtime-main.d649413e.js"
+  }
+]);
